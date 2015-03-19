81ba42ccf192825d201adbe19cc6fb4065930805
ArchAiA <xarchaiax@gmail.com>
2015-03-19 00:09:45 -0400

    Initial Work on Chipotle

--- Class01/chipotle.py
+++ Class01/chipotle.py
@@ -6,7 +6,20 @@ Created on Wed Mar 18 23:11:22 2015
 """
 
 import csv
+import sets
+
 with open('chipotle_orders.tsv') as tsv:
-    data = csv.reader(tsv, delimiter = '\t')
-    print line[3]
-            
+    #data = csv.reader(tsv, delimiter = '\t')
+    data = [row for row in csv.reader(tsv, delimiter = '\t')]
+
+headers = data[0]
+data = data[1:]
+
+orders = set()
+
+for line in data:
+    orders.add(line[0])
+    
+print len(orders)
+
+
